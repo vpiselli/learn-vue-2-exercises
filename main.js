4fc0d7af1648660990ccd71a69c1ce90ec8b7a93
Valerio Piselli <vpiselli@wss.com.do>
2022-03-09 15:19:34 -0400

    Lesson 15: Single-Use Components and Inline Templates

--- main.js
+++ main.js
@@ -1,33 +1,10 @@
-Vue.component('modal', {
-    template: `
-        <div class="modal is-active is-clipped">
-            <div class="modal-background"></div>
-            <div class="modal-card">
-                <header class="modal-card-head">
-                    <p class="modal-card-title">
-                        <slot name="header"></slot>
-                    </p>
-                    <button class="delete" aria-label="close" @click="$emit('close')"></button>
-                </header>
-                <section class="modal-card-body">
-                    <slot>Default content</slot>
-                </section>
-                <footer class="modal-card-foot">
-                    <slot name="footer">
-                        <!-- Default button -->
-                        <button class="button is-success">Ok</button>
-                    </slot>
-                </footer>
-            </div>
-        </div>
-    `
+Vue.component('pregress-view', {
+    data() {
+        return { completionRate: 50 };
+    }
 });
 
 new Vue({
     el: '#root', 
-    
-    data: {
-        isActive: false
-    }
 
 });
