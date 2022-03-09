fc59cd4d797377c628c4687a0e8a75380fe62f36
Valerio Piselli <vpiselli@wss.com.do>
2022-03-09 12:58:33 -0400

    Lesson 14: Named Slots in a Nutshell

--- main.js
+++ main.js
@@ -1,39 +1,33 @@
-// window.Event = new Vue();
-window.Event = new class {
-    constructor() {
-        this.vue = new Vue();
-    }
-
-    fire(event, data = null) {
-        this.vue.$emit(event, data);
-    }
-
-    listen(event, callback) {
-        this.vue.$on(event, callback);
-    }
-}
-
-Vue.component('coupon', {
-    template: `<input placeholder="Enter your coupon code" @blur="onCouponApplied">`,
-
-    methods: {
-        onCouponApplied() {
-            // Event.$emit('applied');
-            Event.fire('applied');
-        }
-    }
+Vue.component('modal', {
+    template: `
+        <div class="modal is-active is-clipped">
+            <div class="modal-background"></div>
+            <div class="modal-card">
+                <header class="modal-card-head">
+                    <p class="modal-card-title">
+                        <slot name="header"></slot>
+                    </p>
+                    <button class="delete" aria-label="close" @click="$emit('close')"></button>
+                </header>
+                <section class="modal-card-body">
+                    <slot>Default content</slot>
+                </section>
+                <footer class="modal-card-foot">
+                    <slot name="footer">
+                        <!-- Default button -->
+                        <button class="button is-success">Ok</button>
+                    </slot>
+                </footer>
+            </div>
+        </div>
+    `
 });
 
 new Vue({
     el: '#root', 
-
+    
     data: {
-        couponApplied: false
-    },
-
-    created() {
-        // Event.$on('applied', () => alert('Handling it!') );
-        Event.listen('applied', () => this.couponApplied = true );
+        isActive: false
     }
 
 });
